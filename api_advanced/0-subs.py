067a3b9ce720b46cc1dd40e6498635da2c5178f4
Terry <156342254+Terrymanzi@users.noreply.github.com>
2024-07-05 05:17:26 -0700

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Script that queries subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
